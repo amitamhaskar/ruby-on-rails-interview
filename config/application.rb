a8b5eddde3be9021ba13fd45e1163bb0db7f8f5f
amita <amitamhaskar@gmail.com>
2019-05-19 18:55:08 +0530

    Initial project commit

---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RailsInterview
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+    config.generators.system_tests = nil
+  end
+end
